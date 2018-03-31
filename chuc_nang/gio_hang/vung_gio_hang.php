a5007da8695084b2ef06aefacdfe9e08a6e19edf
hunghq52 <36027404+hunghq52@users.noreply.github.com>
2018-03-31 20:06:47 +0700

    hung hao hoa

---
+++ chuc_nang/gio_hang/vung_gio_hang.php
@@ -0,0 +1,19 @@
+<br>
+Giỏ hàng <br><br>
+
+<?php 
+	$so_luong=0;
+	if(isset($_SESSION['id_them_vao_gio']))
+	{
+		for($i=0;$i<count($_SESSION['id_them_vao_gio']);$i++)
+		{
+			$so_luong=$so_luong+$_SESSION['sl_them_vao_gio'][$i];
+		}
+	}
+?>
+
+Số sản phẩm : <?php echo $so_luong; ?>
+<br><br>
+<div class="btn">
+<a href="?thamso=gio_hang"><button> Giỏ hàng</button></a>
+</div>
